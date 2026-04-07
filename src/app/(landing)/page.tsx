1a3a43108040666e413c40e4e8e1293443ce752c
heygideon <47083178+heygideon@users.noreply.github.com>
2026-04-07 19:12:18 +0100

    feat(landing): summer 26 logo

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,7 +1,11 @@
 import type { ComponentProps } from "react";
 import Image from "next/image";
 
-import logo from "@/assets/landing/logo.png";
+import ambassador from "@/assets/landing/logo/ambassador.png";
+import become from "@/assets/landing/logo/become.png";
+import becomeArrow from "@/assets/landing/logo/become-arrow.png";
+import summer26Bg from "@/assets/landing/logo/summer-26-bg.png";
+import summer26Fg from "@/assets/landing/logo/summer-26-fg.png";
 import highlight from "@/assets/landing/highlight.svg";
 import flagHoldingText from "@/assets/landing/flagholdingtext.png";
 
@@ -44,12 +48,40 @@ export default function Home() {
     <>
       <div className="p-12 flex gap-8 items-center justify-between bg-grid bg-neutral-50">
         <div>
-          <Image
-            src={logo}
-            alt="Hack Club Ambassador"
-            className="h-24 w-auto"
-          />
-          <h1 className="text-5xl mt-6 font-jersey">
+          <div className="relative w-fit">
+            <Image
+              src={become}
+              alt="Become a"
+              className="h-4 mb-2 -rotate-2 w-auto"
+            />
+            <Image
+              src={becomeArrow}
+              alt=""
+              role="presentation"
+              className="h-8 absolute top-1/2 right-full mr-1 w-auto"
+            />
+          </div>
+          <div className="relative w-fit">
+            <Image
+              src={ambassador}
+              alt="Hack Club Ambassador"
+              className="h-24 w-auto"
+            />
+            <div className="absolute -bottom-5 rotate-2 -right-2">
+              <Image
+                src={summer26Bg}
+                alt=""
+                role="presentation"
+                className="h-8 w-auto"
+              />
+              <Image
+                src={summer26Fg}
+                alt="Summer '26"
+                className="h-4 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-auto"
+              />
+            </div>
+          </div>
+          <h1 className="text-5xl mt-8 font-jersey">
             Inspire more teens like you to code.
           </h1>
           <h1 className="text-5xl relative isolate font-jersey">
